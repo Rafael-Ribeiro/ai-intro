13f3144e58c587a6b15b255498d2e492e4009e50
jprafael <jprafael@student.dei.uc.pt>
2011-03-12 17:59:05 +0000

    demo & template

---
+++ BraitenbergTemplate.py
@@ -0,0 +1,34 @@
+
+# Note: this file was automatically converted to Python from the
+# original steve-language source code.  Please see the original
+# file for more detailed comments and documentation.
+
+
+import breve
+
+class myBraitenbergControl( breve.BraitenbergControl ):
+	def __init__( self ):
+		breve.BraitenbergControl.__init__( self )
+		self.leftSensor = None
+		self.leftWheel = None
+		self.light = None
+		self.rightSensor = None
+		self.rightWheel = None
+		self.vehicle = None
+		myBraitenbergControl.init( self )
+
+	def init( self ):
+		self.light = breve.createInstances( breve.BraitenbergLight, 1 )
+		self.light.move( breve.vector( 10, 1, 0 ) )
+		self.vehicle = breve.createInstances( breve.BraitenbergVehicle, 1 )
+		self.watch( self.vehicle )
+
+
+breve.myBraitenbergControl = myBraitenbergControl
+
+
+# Create an instance of our controller object to initialize the simulation
+
+myBraitenbergControl()
+
+
