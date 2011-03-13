a69cebde992987e57c45a8f1e88c167a4eed24b7
José Ribeiro <jlbribeiro@gmail.com>
2011-03-13 02:38:55 +0000

    BraitenbergActivator sensor linkage

--- src/BraitenbergActivator.py
+++ src/BraitenbergActivator.py
@@ -4,13 +4,15 @@
 #import breve
 
 class BraitenbergActivator(object):
-	def __init__(self, wheel, fn):
+	def __init__(self, wheel, sensors, fn):
 		self.wheel = wheel
 		self.activationFunction = fn
+
 		self.sensors = {}
 
-	def add(self, sensor):
-		self.sensors[sensor.getName()] = 0
+		for sensor in sensors:
+			sensor.link(self)
+			self.sensors[sensor.getName()] = 0
 
 	def activate(self, value, sensor):
 		self.sensors[sensor.getName()] = value
