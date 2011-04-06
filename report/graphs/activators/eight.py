15332831dc635e2b479ca231dafebd4562575e45
jprafael <jprafael@student.dei.uc.pt>
2011-04-07 00:05:19 +0100

    2d Graphs

--- report/graphs/activators/eight.py
+++ report/graphs/activators/eight.py
@@ -19,10 +19,26 @@ yLR = map(lambda x: (RADIUS+AXIS_DIST)*ANGULAR*x, x)
 yRL = map(lambda x: (RADIUS+AXIS_DIST)*ANGULAR*x, x)
 yRR = map(lambda x: RADIUS*ANGULAR*x, x)
 
+X,Y = meshgrid(x,x)
+ZL = X*RADIUS*ANGULAR+Y*(RADIUS+AXIS_DIST)*ANGULAR
+ZR = Y*RADIUS*ANGULAR+X*(RADIUS+AXIS_DIST)*ANGULAR
+
+
 figure(0)
+subplots_adjust(right=0.75)
+
+subplot(2,1,1)
 plot(x, yLL, x, yLR)
 
+subplot(2,1,2)
+pcolor(X,Y,ZL)
+colorbar()
+
 figure(1)
+subplot(2,1,1)
 plot(x, yRL, x, yRR)
 
+subplot(2,1,2)
+pcolor(X,Y,ZR)
+colorbar()
 show()
