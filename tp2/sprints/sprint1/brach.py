f741d8f47d7dbe2898a4f7457b5b262c615f76ea
José Ribeiro <jlbribeiro@gmail.com>
2011-05-24 05:15:21 +0100

    minors

---
+++ tp2/sprints/sprint1/brach.py
@@ -0,0 +1,225 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math, random, copy, sys
+
+DEBUG_MODE = False				# console prints
+
+A = [0.0, 3.0] # initial point
+B = [1.0, 2.0] # end point
+
+DX = float(B[0] - A[0])
+DY = float(B[1] - A[1])
+
+DX_MIN = 0.0001
+DY_MIN = DX_MIN
+
+POPULATION_MAX = 500			# size of population, must be pair
+SELECTION_TYPE = "Tournament" 	# "Roulette"
+
+POINTS_INIT = 30				# max number of points per individual
+ELITISM = 0.1					# percentage
+
+CROSSOVER = 0.05				# probability
+CROSSOVER_LEN_MAX = 0.25		# 1 quarter of the individual is cut
+
+MUTATION_BURST = 0.50			# probability
+MUTATION = 0.1					# percentage
+MUTATION_PROB = 1				# probability	### TODO: Put a spinner on GUI
+MUTATION_Y = 0.2				# percentage of Y mutation
+
+# physical constants
+G_ACC = 9.80655
+
+class Individual:
+	@staticmethod
+	def new(nPoints):
+		# return Individual([[DX/(nPoints-1)*i, (random.random() - 0.5) * 2 * DY + B[1]] for i in xrange(nPoints - 2)] + [[DX/(nPoints-1), B[1]]])
+		return Individual([A] + [[A[0] + DX*i/(nPoints-1), (random.random() - 0.5) * 2 * DY + B[1]] for i in xrange(1,nPoints-1)] + [B])
+
+	# List of n points (2 sized arrays: [abs x, abs y])
+	def __init__(self, points):
+		self.points = points
+		self.fitness_val = None
+
+	def _findXCoord(self, x): # returns a tuple (index, splitNeeded)
+		for i in xrange(len(self.points)):
+			if self.points[i][0] >= x:
+				return i, self.points[i][0] != x
+
+		print "_findXCoord Error: %f exceeds boundaries (%f)" %(x, self.points[-1][0])
+		sys.exit(-1)
+
+		return -1
+
+	def _splitXCoord(self, x):
+		xIndex, splitNeeded = self._findXCoord(x)
+
+		if not splitNeeded:
+			return xIndex
+
+		self.points.insert(xIndex,[x, self.points[xIndex][1]])
+
+		return xIndex
+
+	def crossover(self, other, xInit, xEnd):
+		xi1, xj1 = self.crossoverSegment(xInit, xEnd)
+		xi2, xj2 = other.crossoverSegment(xInit, xEnd)
+
+		tempPoints = self.points[:xi1] + other.points[xi2:xj2] + self.points[xj1:]
+		other.points = other.points[:xi2] + self.points[xi1:xj1] + other.points[xj2:]
+		self.points = tempPoints
+
+		self.fitness_val = None
+		other.fitness_val = None
+	
+	def crossoverSegment(self, xInit, xEnd):
+		return self._splitXCoord(xInit), self._splitXCoord(xEnd) + 1
+
+	def fitness(self):
+		if self.fitness_val:
+			return self.fitness_val
+
+		x_i = A[0]
+		y_i = maxHeight = A[1]
+		v_i = time = 0.0
+		
+		#It has to have at least 2 points, otherwise it is not doable
+		if len(self.points) < 2:
+			return -1;
+		
+		for i in range(1, len(self.points)):
+			if self.points[i][1] > maxHeight or (self.points[i][1] == maxHeight and i != len(self.points)):
+				print "c", i, maxHeight, self.points[i][1]
+				return -1
+			
+			#Calculate acceleration based on segment slope. Open question - would be using cos() faster?
+			#Since the angle had to be calculated beforehand, I don't think so
+			dx = self.points[i][0] - x_i
+			dy = self.points[i][1] - y_i
+			li = math.sqrt(dx*dx + dy*dy)
+			ai = -G_ACC*dy/float(li)
+			
+			#Energy conservation. You really needn't be reading this, but if you have doubts, tell me
+			v_j = math.sqrt(2*(-G_ACC*dy + v_i*v_i/2))
+			#If speed is less than 0, forget about it. This actually never happens thanks to our checks.
+			#But you never know what magic lies in wait...
+
+			if v_j <= 0.0:
+				sys.exit(-1)
+				return -1
+
+			dv = v_j - v_i
+			
+			#The speed variation over the acceleration gives us the time. Voila
+			time += dv/ai if ai != 0 else li/v_j
+
+			#Debugging, ignore
+			if DEBUG_MODE:
+				print "Segment " + str(i/2)
+				print "dX: " + str(dx) + " dY: " + str(dy)
+				print "Distance Travelled: " + str(li)
+				print "Gained velocity: " + str(dv)
+				print "Acceleration: " + str(ai)
+				print "Time to travel: " + time +"\n"
+			
+			#Update the positions and current speed
+			v_i = v_j
+			x_i = self.points[i][0]
+			y_i = self.points[i][1]
+		
+		self.fitness_val = time
+		return time
+
+	def getPlotData(self):
+		x = [i[0] for i in self.points]
+		y = [i[1] for i in self.points]
+
+		return x, y
+
+	def mutate(self):
+		# dont mutate first and last
+		k = random.randint(1,max(1,int(MUTATION * (len(self.points) - 2))))
+
+		mutations = random.sample(range(1, len(self.points)-1), k)
+
+		for mutIndex in mutations:
+			window = self.points[mutIndex+1][0] - self.points[mutIndex-1][0] - 2*DX_MIN
+			self.points[mutIndex][0] = self.points[mutIndex-1][0] + DX_MIN + random.random()*window
+			
+			self.points[mutIndex][1] += (random.random() - 0.5) * 2 * MUTATION_Y * DY
+			self.points[mutIndex][1] = min(self.points[mutIndex][1], A[1] - DY_MIN)
+
+		self.fitness_val = None
+
+class Population:
+	@staticmethod
+	def new(nIndividuals):
+		individuals = [Individual.new(POINTS_INIT) for i in xrange(nIndividuals)]
+
+		return Population(individuals)
+
+	def __init__(self, individuals):
+		self.individuals = individuals
+
+	def evolve(self):
+		individuals = [] # next population
+
+		# reproduce (with cross-over)
+		random.shuffle(self.individuals)
+		for i in xrange(len(self.individuals)/2):
+			individual1 = copy.deepcopy(self.individuals[i*2])
+			individual2 = copy.deepcopy(self.individuals[(i*2)+1])
+
+			prob = random.random()
+			if prob <= CROSSOVER: # crossover both individuals (else: clone them)
+				crossoverMaxLen = random.random() * CROSSOVER_LEN_MAX * DX
+
+				startingCrossoverPoint = random.random() * (DX - crossoverMaxLen)
+				endingCrossoverPoint = startingCrossoverPoint + crossoverMaxLen
+
+				individual1.crossover(individual2, startingCrossoverPoint, endingCrossoverPoint)
+
+			individuals.append(individual1)
+			individuals.append(individual2)
+
+		if len(self.individuals) % 2 != 0: # forever alone: survives without change
+			individuals.push(copy.deepcopy(self.individuals[-1]))
+
+		# mutate childs
+		for individual in individuals:
+			if random.random() <= MUTATION_PROB:
+				individual.mutate()
+
+		# join parents and childs
+		individuals += self.individuals
+		individuals.sort(key = Individual.fitness)
+
+		# elitism selection
+		cutoff = int(ELITISM * len(individuals))
+
+		self.individuals = individuals[:cutoff]
+		individuals = individuals[cutoff:]
+
+		# tournament/roulette selection
+
+		self.individuals += individuals[len(self.individuals):POPULATION_MAX] # TODO: its here just while there is no selection
+		
+	def getBest(self):
+		return self.individuals[0]
+
+	def getStatistics(self):
+		max_fitness = self.individuals[0].fitness()
+		min_fitness = self.individuals[-1].fitness()
+
+		avg_fitness = 0.0
+		for i in self.individuals:
+			avg_fitness += i.fitness()
+		avg_fitness /= POPULATION_MAX
+
+		sqr_sum = 0.0
+		for i in self.individuals:
+			sqr_sum += (i.fitness() - avg_fitness)**2
+
+		dev_fitness = math.sqrt(sqr_sum/POPULATION_MAX)
+		return max_fitness, avg_fitness, min_fitness, dev_fitness
---
+++ tp2/sprints/sprint1/gui.py
@@ -0,0 +1,299 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import signal, os
+
+import brach
+import sys
+from threading import Thread
+
+from datetime import datetime, timedelta
+
+import matplotlib.pyplot as plot
+from matplotlib.backends.backend_gtk import Figure, FigureCanvasGTK
+
+# program constants
+GUI_FILENAME = "gui.xml"
+
+try:
+	import gtk
+	import gtk.glade
+except:
+	print "You need to install pyGTK or GTK"
+	sys.exit(1)
+
+class BrachGUI:
+	def __init__(self):
+		self.lastUpdate = datetime.now()
+		self.thread = None
+		self.lastBest = None
+
+		self.builder = gtk.Builder()
+		self.builder.add_from_file(GUI_FILENAME)
+
+		# init default values (due to Glade 3.7 bug)
+		self.builder.get_object("input_Ax").set_value(brach.A[0])
+		self.builder.get_object("input_Ay").set_value(brach.A[1])
+		self.builder.get_object("input_Bx").set_value(brach.B[0])
+		self.builder.get_object("input_By").set_value(brach.B[1])
+
+		self.builder.get_object("input_population_size").set_value(brach.POPULATION_MAX)
+		self.builder.get_object("input_elitism").set_value(brach.ELITISM*100)
+		self.builder.get_object("input_points").set_value(brach.POINTS_INIT)
+		self.builder.get_object("input_crossover").set_value(brach.CROSSOVER*100)
+		self.builder.get_object("input_crossover_len").set_value(brach.CROSSOVER_LEN_MAX*100)
+		self.builder.get_object("input_mutation").set_value(brach.MUTATION*100)
+		self.builder.get_object("input_mutation_y").set_value(brach.MUTATION_Y*100)
+		self.builder.get_object("input_mutation_burst").set_value(brach.MUTATION_BURST*100)
+
+		# init the input_selection_type
+		selection_model = gtk.ListStore(str)
+		selection_model.append(["Tournament"])
+		selection_model.append(["Roulette"])
+
+		selection_box = self.builder.get_object("input_selection_type")
+		selection_box.set_model(selection_model)
+
+		cell = gtk.CellRendererText()
+		selection_box.pack_start(cell)
+		selection_box.add_attribute(cell,'text',0)
+		selection_box.set_active(0)
+
+		self.builder.get_object("button_save").set_sensitive(False)
+		self.running = False
+
+		# init graphs
+		self.fig_best = Figure(figsize=(400, 400))
+		self.fig_hist = Figure(figsize=(400, 200))
+		
+		self.canvas_best = FigureCanvasGTK(self.fig_best)
+		self.canvas_hist = FigureCanvasGTK(self.fig_hist)
+		self.canvas_best.show()
+		self.canvas_hist.show()
+
+		self.hbox_best = self.builder.get_object("hbox_graph_best")
+		self.hbox_hist = self.builder.get_object("hbox_graph_hist")
+
+		self.hbox_best.pack_start(self.canvas_best, True, True)
+		self.hbox_hist.pack_start(self.canvas_hist, True, True)
+
+		# show window
+		self.builder.connect_signals(self)
+		window = self.builder.get_object('window_main')
+		window.set_title("Brachistochrone curve")
+		window.show()
+
+	def on_adjust_Ax_value_changed(self, widget, data=None):
+		brach.A[0] = widget.get_value()
+
+		if brach.A[0] >= brach.B[0]:
+			widget.set_value(brach.A[0]-0.1)
+
+		return True
+
+	def on_adjust_Ay_value_changed(self, widget, data=None):
+		brach.A[1] = widget.get_value()
+
+		if brach.A[1] < brach.B[1]:
+			widget.set_value(brach.A[1] + 0.1)
+
+		return True
+
+	def on_adjust_Bx_value_changed(self, widget, data=None):
+		brach.B[0] = widget.get_value()
+
+		if brach.B[0] <= brach.A[0]:
+			widget.set_value(brach.B[0] + 0.1)
+
+		return True
+
+	def on_adjust_By_value_changed(self, widget, data=None):
+		brach.A[1] = widget.get_value()
+
+		if brach.B[1] > brach.A[1]:
+			widget.set_value(brach.B[1] - 0.1)
+
+		return True
+
+	def on_input_population_size_value_changed(self, widget, data=None):
+		brach.POPULATION_MAX = widget.get_value_as_int()
+		return True
+
+	def on_input_elitism_value_changed(self, widget, data=None):
+		brach.ELITISM = widget.get_value()/100
+		return True
+
+	def on_input_selection_type_changed(self, widget, data=None):
+		brach.SELECTION_TYPE = widget.get_active_text()
+		return True
+
+	def on_input_points_value_changed(self, widget, data=None):
+		brach.POINTS_INIT = widget.get_value_as_int()
+		return True
+
+	def on_input_crossover_value_changed(self, widget, data=None):
+		brach.CROSSOVER = widget.get_value()/100
+		return True
+
+	def on_input_crossover_len_value_changed(self, widget, data=None):
+		brach.CROSSOVER_LEN_MAX = widget.get_value()/100
+		return True
+
+	def on_input_mutation_value_changed(self, widget, data=None):
+		brach.MUTATION = widget.get_value()/100
+		return True
+
+	def on_input_mutation_y_value_changed(self, widget, data=None):
+		brach.MUTATION_Y = widget.get_value()/100
+		return True
+
+	def on_input_mutation_burst_value_changed(self, widget, data=None):
+		brach.MUTATION_BURST = widget.get_value()/100
+		return True
+
+	def on_button_save_clicked(self, widget, data=None):
+		# TODO: http://www.pygtk.org/pygtk2tutorial/sec-FileChoosers.html
+		# chooser.set_action(gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER)
+
+		dialog = gtk.FileChooserDialog("Choose a folder..",
+			None,
+			gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER,
+			(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK)
+		)
+		dialog.set_default_response(gtk.RESPONSE_OK)
+
+		response = dialog.run()
+		if response == gtk.RESPONSE_OK:
+			points = self.population.getBest().getPlotData()
+
+			f = open(dialog.get_filename() + '/data', 'w')
+			f.write(str(self.iteration_list[-1])+"\n")
+			f.write(str(self.best_list)+"\n")
+			f.write(str(self.avg_list)+"\n")
+			f.write(str(self.worst_list)+"\n")
+			f.write(str(self.stddev_list)+"\n")
+			f.write(str(points)+"\n")
+			f.close()
+
+			figureBest = plot.Figure(figsize=(400,400), dpi=72)
+			graphBest = figureBest.add_subplot(111)
+			graphBest.plot(points[0], points[1], 'r-*')
+			graphBest.axis([0, brach.B[0], 0, brach.A[1]])
+			figureBest.savefig(dialog.get_filename() + '/best.png')
+
+		dialog.destroy()
+		return True
+
+	def on_button_start_stop_clicked(self, widget, data=None):
+		self.running = not self.running
+		
+		if self.running:
+			widget.set_label("STOP")
+			
+			self.best_list = []
+			self.avg_list = []
+			self.worst_list = []
+			self.stddev_list = []
+			self.iteration_list = []
+
+			self.builder.get_object("input_Ax").set_sensitive(False)
+			self.builder.get_object("input_Ay").set_sensitive(False)
+			self.builder.get_object("input_Bx").set_sensitive(False)
+			self.builder.get_object("input_By").set_sensitive(False)
+
+			self.builder.get_object("input_population_size").set_sensitive(False)
+			self.builder.get_object("input_elitism").set_sensitive(False)
+			self.builder.get_object("input_selection_type").set_sensitive(False)
+
+			self.builder.get_object("input_points").set_sensitive(False)
+			self.builder.get_object("input_crossover").set_sensitive(False)
+			self.builder.get_object("input_crossover_len").set_sensitive(False)
+			self.builder.get_object("input_mutation").set_sensitive(False)
+			self.builder.get_object("input_mutation_y").set_sensitive(False)
+			self.builder.get_object("input_mutation_burst").set_sensitive(False)
+			
+			self.builder.get_object("button_save").set_sensitive(False)
+
+			self.population = brach.Population.new(brach.POPULATION_MAX)
+
+			self.thread = Thread(target=self.evolve)
+			self.thread.start()
+
+		else:
+			widget.set_label("START")
+
+			self.builder.get_object("input_Ax").set_sensitive(True)
+			self.builder.get_object("input_Ay").set_sensitive(True)
+			self.builder.get_object("input_Bx").set_sensitive(True)
+			self.builder.get_object("input_By").set_sensitive(True)
+
+			self.builder.get_object("input_population_size").set_sensitive(True)
+			self.builder.get_object("input_elitism").set_sensitive(True)
+			self.builder.get_object("input_selection_type").set_sensitive(True)
+
+			self.builder.get_object("input_points").set_sensitive(True)
+			self.builder.get_object("input_crossover").set_sensitive(True)
+			self.builder.get_object("input_crossover_len").set_sensitive(True)
+			self.builder.get_object("input_mutation").set_sensitive(True)
+			self.builder.get_object("input_mutation_y").set_sensitive(True)
+			self.builder.get_object("input_mutation_burst").set_sensitive(True)
+
+			self.builder.get_object("button_save").set_sensitive(True)
+			self.lastBest = None
+
+		return True
+
+	def on_window_main_destroy(self, widget, data=None):
+		app.running = False
+		if app.thread != None:
+			app.thread.join()
+		sys.exit(0)
+
+	def evolve(self):
+		i = 0
+		while self.running:
+			self.population.evolve()
+	
+			stats = self.population.getStatistics()
+			print "Iteration", i, stats
+
+			self.best_list.append(stats[0])
+			self.avg_list.append(stats[1])
+			self.worst_list.append(stats[2])
+			self.stddev_list.append(stats[3])
+			self.iteration_list.append(i)
+
+			if datetime.now() > self.lastUpdate + timedelta(seconds=1):
+				self.lastUpdate = datetime.now()
+
+				# best solution
+				points = self.population.getBest().getPlotData()
+
+				self.fig_best.clf()
+				self.fig_hist.clf()
+
+				graph_best = self.fig_best.add_subplot(111)
+				graph_best.plot(points[0], points[1], 'r-*')
+				graph_best.axis([0, brach.B[0], 0, brach.A[1]])
+
+				graph_hist = self.fig_hist.add_subplot(111)
+				graph_hist.plot(self.iteration_list[-100:], self.best_list[-100:], 'b', self.iteration_list[-100:], self.avg_list[-100:], 'g', self.iteration_list[-100:], self.worst_list[-100:], 'r')
+
+				self.fig_best.canvas.draw()
+				self.fig_hist.canvas.draw()
+
+			i += 1
+
+if __name__ == '__main__':
+	gtk.gdk.threads_init()
+
+	app = BrachGUI()
+
+	try:
+		gtk.main()
+
+	except KeyboardInterrupt:
+		app.running = False
+		if app.thread != None:
+			app.thread.join()
+		sys.exit(0)
