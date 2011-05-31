e02063e3ab587d2bc036121b900cfdbbf1c1624a
jprafael <jprafael@student.dei.uc.pt>
2011-05-31 01:11:31 +0100

    rollback

--- tp2/src/test.py
+++ tp2/src/test.py
@@ -15,14 +15,6 @@ if len(sys.argv) < 3:
 else:
 	SEEDS = range(int(sys.argv[1]), int(sys.argv[2])+1)
 
-def listsum(a, b)
-	for i in xrange(len(a)):
-		a[i] += b[i]
-
-def listdiv(a, q)
-	for i in xrange(len(a)):
-		a[i] /= q
-
 # parameters to be tested
 CROSSOVER_PROBS = [0.0, 0.35]										# probabilities
 CROSSOVER_POINTS = [2, 5]											# number of points that are cut
@@ -76,97 +68,117 @@ if __name__ == '__main__':
 		config.DY = float(config.B[1] - config.A[1])
 
 		initial_path = '../results/initial_%d' % (initial,)
+		make_dir(initial_path)
+		#if os.path.isfile(repr_path + "/.done"):
+		#	continue
 
 		for representation in REPRESENTATIONS:
 			config.REPRESENTATION = representation
 		
 			repr_path = '%s/%s' % (initial_path, representation.split()[0].lower())
+			make_dir(repr_path)
+			#if os.path.isfile(repr_path + "/.done"):
+			#	continue
 
 			for selection_type in SELECTION_TYPES:
 				config.SELECTION_TYPE = selection_type
 
 				selection_types_path = "%s/%s" % (repr_path, selection_type.lower())
+				make_dir(selection_types_path)
+				#if os.path.isfile(selection_types_path + "/.done"):
+				#	continue
 
 				for points in POINTS:
 					config.POINTS_INIT = points
 
 					points_path = '%s/%d_points' % (selection_types_path, points)
+					make_dir(points_path)
+					#if os.path.isfile(points_path + "/.done"):
+					#	continue
 
 					for population_size in POPULATION_SIZES:
 						config.POPULATION_SIZE = population_size
 
 						population_sizes_path = "%s/%d_pop" %(points_path, population_size)
+						make_dir(population_sizes_path)
+						#if os.path.isfile(population_sizes_path + "/.done"):
+						#	continue
 
 						for elitism in ELITISMS:
 							config.ELITISM = elitism
 
 							elitisms_path = "%s/%.2f_elite" % (population_sizes_path, elitism)
+							make_dir(elitisms_path)
+							#if os.path.isfile(elitisms_path + "/.done"):
+							#	continue
 
 							for crossover_prob in CROSSOVER_PROBS:
 								config.CROSSOVER_PROB = crossover_prob
 
 								crossover_prob_path = "%s/%.2f_cross_prob" % (elitisms_path, crossover_prob)
+								make_dir(crossover_prob_path)
+								#if os.path.isfile(crossover_prob_path + "/.done"):
+								#	continue
 
 								for crossover_points in CROSSOVER_POINTS:
 									config.CROSSOVER_POINTS = crossover_points
 
 									crossover_points_path = "%s/%d_cross_points" % (crossover_prob_path, crossover_points)
+									make_dir(crossover_points_path)
+									#if os.path.isfile(crossover_len_maxs_path + "/.done"):
+									#	continue
 
 									for mutation_prob in MUTATION_PROBS:
 										config.MUTATION_PROB = mutation_prob
 
 										mutation_probs_path = "%s/%.2f_mut_prob" % (crossover_points_path, mutation_prob)
+										make_dir(mutation_probs_path)
+										#if os.path.isfile(mutation_probs_path + "/.done"):
+										#	continue
 
-										# TODO:
-										# render average graphs for each of the population sizes
-										# render the best individual for all seeds for the population size
-										bests = [-1 for i in xrange(MAX_ITERATIONS)]
-										best_val [float("inf") for i in xrange(MAX_ITERATIONS)]
-
-										best_list = [0.0 for i in xrange(MAX_ITERATIONS)]
-										avg_list = [0.0 for i in xrange(MAX_ITERATIONS)]
-										worst_list = [0.0 for i in xrange(MAX_ITERATIONS)]
-										stddev_list = [0.0 for i in xrange(MAX_ITERATIONS)]
-										
 										for seed in SEEDS:
-											seed_path = "%s/%d/%d/data" % (mutation_probs_path, seed, MAX_ITERATIONS)
-											
-											f = open(seed_path, 'r')
-											f.readline() # nr of iterations 
+											random.seed(seed)
 
-											this_best = eval(f.readline())
-											for i in xrange(len(this_best)):
-												if this_best[i] > best_val[i]:
-													bests[i] = seed
-													best_val[i] = this_best[i]
+											seed_path = "%s/%d" % (mutation_probs_path, seed)
+											make_dir(seed_path)
+											if os.path.isfile(seed_path + "/.done"):
+												continue
 
-											listsum(best_list, this_best)
+											best_list = []
+											avg_list = []
+											worst_list = []
+											stddev_list = []
+										
+											start = time.time()
+											population = Population.new(config.POPULATION_SIZE, config.REPRESENTATION)
+											for i in xrange(1, MAX_ITERATIONS+1):
+												population.evolve()
 
-											this_avg = eval(f.readline())
-											listsum(avg_list, this_avg)
+												stats = population.getStatistics()
 
-											this_worst = eval(f.readline())
-											listsum(worst_list, this_worst)
+												best_list.append(stats[0])
+												avg_list.append(stats[1])
+												worst_list.append(stats[2])
+												stddev_list.append(stats[3])
+								
+												if i in ITERATIONS:
+													final_path = "%s/%d" % (seed_path, i)
+													best = population.getBest().getPoints()
 
-											this_stddev = eval(f.readline())
-											listsum(stddev_list, this_stddev)
+													make_dir(final_path)
+													end = time.time()
+													save_data(final_path, best, best_list, avg_list, worst_list, stddev_list, xrange(1, i+1), end-start)
+											print "finished test: ", seed_path
 
-											# do not need points for now
-											f.close()
+											open(seed_path + "/.done", 'w').close()
+	"""									open(mutation_probs_path + "/.done", 'w').close()
+									open(crossover_len_maxs_path + "/.done", 'w').close()
+								open(crossover_prob_path + "/.done", 'w').close()
+							open(elitisms_path + "/.done", 'w').close()
+						open(population_sizes_path + "/.done", 'w').close()
+					open(points_path + "/.done", 'w').close()
+				open(selection_types_path + "/.done", 'w').close()
+			open(repr_path + "/.done", 'w').close()
+	"""
 
-										listdiv(best_list, 30.0)
-										listdiv(avg_list, 30.0)
-										listdiv(worst_list, 30.0)
-										listdiv(stddev_list, 30.0)
-
-										for i in ITERATIONS:
-											seed_path = "%s/%d/%d/data" % (mutation_probs_path, seed, bests[i-1])
-											f = open(seed_path, 'r')
-											lines = f.readlines()
-
-											points = eval(lines[5]) # points
-											# best_val[i-1] --> fitness
-											# plot this
-
-										print "finished graphs: ", mutation_probs_path 
 	print "FINISHED!"
